f4c43bfed5875cc954f190bc6caa2154f1480a0d
kaineomg <mgt62rus@gmail.com>
2021-09-13 19:04:10 +0300

    rs

---
+++ Card.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Card
+    {
+       
+        public enum val
+        {
+            one = 1,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight,
+            nine,
+            ten,
+            valet,
+            queen,
+            king,
+            ace
+        }
+        public enum suit
+        {
+            spades,
+            clubs,
+            hearts,
+            diamonds
+        }
+        public suit Suit { get; private set; }
+        public val Value { get; private set; }
+    Card (int val, int mas)
+        {
+            Suit = mas;
+            Value = val; 
+           
+        }
+
+
+
+
+    }
+}
